d2a69fd1ef3e9e36bf4c82f5ec7d6d72cc4b1466
Anders Houbak Kristiansen <palantus@gmail.com>
2014-08-11 16:59:41 +0200

    Initial upload

---
+++ MailModule.js
@@ -0,0 +1,36 @@
+var redis = require("redis"),
+client = redis.createClient();
+
+var MailModule = function () {
+};
+
+MailModule.prototype.init = function(fw, onFinished) {
+    this.fw = fw;
+	onFinished.call(this);
+}
+
+MailModule.prototype.onMessage = function (req, callback) {
+	if(typeof(req.body.type) !== "string"){
+		callback({error: "Invalid request"});
+		return;
+	}
+
+	switch(req.body.type){
+		case "GetRecentMailList" :
+			client.zrange("mails:ankri.dk:ahk", 0, -1, function(err, mailIds){
+				var multi = client.multi();
+
+				mailIds.forEach(function (id, i) {
+		            multi.hgetall("mail:" + id);
+		        });
+				
+				multi.exec(function(err, mails){
+					callback(mails);
+				});
+			});
+			
+			break;
+	}
+};		
+ 
+module.exports = MailModule;
